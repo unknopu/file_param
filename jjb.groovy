f84c9dd4e3ecf54efc14a7c53f38bda5d0b06791
lin <unknopu@gmail.com>
2023-06-27 11:57:10 +0700

    reinit param, template

---
+++ jjb.groovy
@@ -0,0 +1,20 @@
+pipeline {
+    agent { 
+        label: 'slave-2'
+    }
+
+    stages {
+        stage('Prepare job definition') {
+            steps {
+                script {
+                    cleanWs()
+
+                    pipelineConfig = readYaml file: "${WORKSPACE}/params/${params.pipeline_parameter_file_path}"
+                    // jenkinsJobTemplate = "${pipelineConfig.parameters.general.jenkinsJobTemplate}"
+
+                    echo "${pipelineConfig.test}"
+                }
+            }
+        }
+    }
+}
